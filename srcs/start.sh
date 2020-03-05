d7ee0c42b9cfbff2cf16954dd4fe2042f7582044
Pierre-marie LAM <plam@e2r12p5.42.fr>
2020-03-05 11:24:32 +0100

    first deposit

--- srcs/start.sh
+++ srcs/start.sh
@@ -2,6 +2,13 @@
 
 service nginx start \
 && service mysql start \
+&& mysql -u root -e "CREATE DATABASE wordpress_database" \
+	&& mysql -u root -e "CREATE USER 'plam'@'localhost' IDENTIFIED BY 'oof'" \
+	&& mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress_database.* TO 'plam'@'localhost' IDENTIFIED BY 'oof'" \
+	&& mysql -u root -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO 'plam'@'localhost' IDENTIFIED BY 'oof'" \
+	&& mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'plam'@'localhost' IDENTIFIED BY 'oof'" \
+	&& mysql -u root -e "FLUSH PRIVILEGES" \
+	&& mysql wordpress_database < /tmp/wordpress_database.sql\
 && service php7.3-fpm start
 
 bash
