984a503fedb883aa061ad0ed03d4b8f6db4f20dd
Pierre-marie LAM <plam@e3r5p8.42.fr>
2020-03-07 12:34:20 +0100

    first deposit

--- srcs/start.sh
+++ srcs/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [AUTOINDEX = ON]; then
-	cp srcs/nginx_on.conf /etc/nginx/sites-available/localhost
-else
-	cp srcs/nginx_off.conf /etc/nginx/sites-available/localhost
-fi
-
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
+
+if [AUTOINDEX = ON]; then
+	mv srcs/nginx_on.conf /etc/nginx/sites-available/localhost/nginx.conf
+else
+	mv srcs/nginx_off.conf /etc/nginx/sites-available/localhost/nginx.conf
+fi
 
 service nginx start
 service mysql start
